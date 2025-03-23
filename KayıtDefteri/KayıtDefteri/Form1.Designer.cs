f9270047e104e60116aa35883c80ca9028a98622
EmirCoban <125053917+EmirCobanOfficial@users.noreply.github.com>
2025-03-24 00:27:29 +0300

    Add files via upload

---
+++ KayıtDefteri/KayıtDefteri/Form1.Designer.cs
@@ -0,0 +1,294 @@
+﻿namespace KayıtDefteri
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            button1 = new Button();
+            label1 = new Label();
+            textBox1 = new TextBox();
+            label2 = new Label();
+            label3 = new Label();
+            textBox2 = new TextBox();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            comboBox1 = new ComboBox();
+            label7 = new Label();
+            label8 = new Label();
+            label9 = new Label();
+            label10 = new Label();
+            textBox3 = new TextBox();
+            label11 = new Label();
+            label12 = new Label();
+            label13 = new Label();
+            label14 = new Label();
+            textBox4 = new TextBox();
+            label15 = new Label();
+            SuspendLayout();
+            // 
+            // button1
+            // 
+            button1.Location = new Point(137, 388);
+            button1.Name = "button1";
+            button1.Size = new Size(149, 50);
+            button1.TabIndex = 0;
+            button1.Text = "Kaydet";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(47, 41);
+            label1.Name = "label1";
+            label1.Size = new Size(84, 20);
+            label1.TabIndex = 1;
+            label1.Text = "Adı Soyadı:";
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(137, 41);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(413, 48);
+            label2.Name = "label2";
+            label2.Size = new Size(84, 20);
+            label2.TabIndex = 3;
+            label2.Text = "Adı Soyadı:";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(522, 48);
+            label3.Name = "label3";
+            label3.Size = new Size(25, 20);
+            label3.TabIndex = 4;
+            label3.Text = "00";
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(137, 87);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(125, 27);
+            textBox2.TabIndex = 6;
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(59, 87);
+            label4.Name = "label4";
+            label4.Size = new Size(58, 20);
+            label4.TabIndex = 5;
+            label4.Text = "Telefon";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(522, 94);
+            label5.Name = "label5";
+            label5.Size = new Size(25, 20);
+            label5.TabIndex = 8;
+            label5.Text = "00";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(413, 94);
+            label6.Name = "label6";
+            label6.Size = new Size(61, 20);
+            label6.TabIndex = 7;
+            label6.Text = "Telefon:";
+            // 
+            // comboBox1
+            // 
+            comboBox1.FormattingEnabled = true;
+            comboBox1.Items.AddRange(new object[] { "Manisa", "Demirci", "Borlu", "Köprübası", "Turgutlu", "Kadıköy", "Beyoğlu" });
+            comboBox1.Location = new Point(137, 130);
+            comboBox1.Name = "comboBox1";
+            comboBox1.Size = new Size(125, 28);
+            comboBox1.TabIndex = 9;
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(71, 138);
+            label7.Name = "label7";
+            label7.Size = new Size(46, 20);
+            label7.TabIndex = 10;
+            label7.Text = "Semt:";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(522, 138);
+            label8.Name = "label8";
+            label8.Size = new Size(25, 20);
+            label8.TabIndex = 12;
+            label8.Text = "00";
+            // 
+            // label9
+            // 
+            label9.AutoSize = true;
+            label9.Location = new Point(413, 138);
+            label9.Name = "label9";
+            label9.Size = new Size(46, 20);
+            label9.TabIndex = 11;
+            label9.Text = "Semt:";
+            // 
+            // label10
+            // 
+            label10.AutoSize = true;
+            label10.Location = new Point(71, 176);
+            label10.Name = "label10";
+            label10.Size = new Size(50, 20);
+            label10.TabIndex = 14;
+            label10.Text = "Adres:";
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(137, 176);
+            textBox3.Multiline = true;
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(125, 27);
+            textBox3.TabIndex = 15;
+            // 
+            // label11
+            // 
+            label11.AutoSize = true;
+            label11.Location = new Point(522, 179);
+            label11.Name = "label11";
+            label11.Size = new Size(25, 20);
+            label11.TabIndex = 17;
+            label11.Text = "00";
+            // 
+            // label12
+            // 
+            label12.AutoSize = true;
+            label12.Location = new Point(413, 179);
+            label12.Name = "label12";
+            label12.Size = new Size(50, 20);
+            label12.TabIndex = 16;
+            label12.Text = "Adres:";
+            // 
+            // label13
+            // 
+            label13.AutoSize = true;
+            label13.Location = new Point(522, 224);
+            label13.Name = "label13";
+            label13.Size = new Size(25, 20);
+            label13.TabIndex = 21;
+            label13.Text = "00";
+            // 
+            // label14
+            // 
+            label14.AutoSize = true;
+            label14.Location = new Point(413, 224);
+            label14.Name = "label14";
+            label14.Size = new Size(71, 20);
+            label14.TabIndex = 20;
+            label14.Text = "Tc Kimlik:";
+            // 
+            // textBox4
+            // 
+            textBox4.Location = new Point(137, 221);
+            textBox4.Multiline = true;
+            textBox4.Name = "textBox4";
+            textBox4.Size = new Size(125, 27);
+            textBox4.TabIndex = 19;
+            // 
+            // label15
+            // 
+            label15.AutoSize = true;
+            label15.Location = new Point(59, 221);
+            label15.Name = "label15";
+            label15.Size = new Size(75, 20);
+            label15.TabIndex = 18;
+            label15.Text = "Tc Klimlik:";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(800, 450);
+            Controls.Add(label13);
+            Controls.Add(label14);
+            Controls.Add(textBox4);
+            Controls.Add(label15);
+            Controls.Add(label11);
+            Controls.Add(label12);
+            Controls.Add(textBox3);
+            Controls.Add(label10);
+            Controls.Add(label8);
+            Controls.Add(label9);
+            Controls.Add(label7);
+            Controls.Add(comboBox1);
+            Controls.Add(label5);
+            Controls.Add(label6);
+            Controls.Add(textBox2);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(textBox1);
+            Controls.Add(label1);
+            Controls.Add(button1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Button button1;
+        private Label label1;
+        private TextBox textBox1;
+        private Label label2;
+        private Label label3;
+        private TextBox textBox2;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private ComboBox comboBox1;
+        private Label label7;
+        private Label label8;
+        private Label label9;
+        private Label label10;
+        private TextBox textBox3;
+        private Label label11;
+        private Label label12;
+        private Label label13;
+        private Label label14;
+        private TextBox textBox4;
+        private Label label15;
+    }
+}
