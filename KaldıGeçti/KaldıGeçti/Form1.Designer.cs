92a01187facf66b5a54ff729ab2c1fbf14c3949a
EmirCoban <125053917+EmirCobanOfficial@users.noreply.github.com>
2025-03-24 00:25:15 +0300

    Add files via upload

---
+++ KaldıGeçti/KaldıGeçti/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿namespace KaldıGeçti
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            label1 = new Label();
+            label2 = new Label();
+            label3 = new Label();
+            label4 = new Label();
+            label5 = new Label();
+            label6 = new Label();
+            label7 = new Label();
+            label8 = new Label();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            listBox3 = new ListBox();
+            listBox4 = new ListBox();
+            listBox5 = new ListBox();
+            textBox1 = new TextBox();
+            textBox2 = new TextBox();
+            textBox3 = new TextBox();
+            button1 = new Button();
+            SuspendLayout();
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(12, 63);
+            label1.Name = "label1";
+            label1.Size = new Size(102, 20);
+            label1.TabIndex = 0;
+            label1.Text = "Kullanıcı Adı=";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(47, 134);
+            label2.Name = "label2";
+            label2.Size = new Size(56, 20);
+            label2.TabIndex = 1;
+            label2.Text = "Sınav 1";
+            // 
+            // label3
+            // 
+            label3.AutoSize = true;
+            label3.Location = new Point(47, 195);
+            label3.Name = "label3";
+            label3.Size = new Size(56, 20);
+            label3.TabIndex = 2;
+            label3.Text = "Sınav 2";
+            // 
+            // label4
+            // 
+            label4.AutoSize = true;
+            label4.Location = new Point(22, 373);
+            label4.Name = "label4";
+            label4.Size = new Size(81, 20);
+            label4.TabIndex = 3;
+            label4.Text = "Adı Soyadı";
+            // 
+            // label5
+            // 
+            label5.AutoSize = true;
+            label5.Location = new Point(266, 373);
+            label5.Name = "label5";
+            label5.Size = new Size(56, 20);
+            label5.TabIndex = 4;
+            label5.Text = "Sınav 1";
+            // 
+            // label6
+            // 
+            label6.AutoSize = true;
+            label6.Location = new Point(444, 373);
+            label6.Name = "label6";
+            label6.Size = new Size(56, 20);
+            label6.TabIndex = 5;
+            label6.Text = "Sınav 2";
+            // 
+            // label7
+            // 
+            label7.AutoSize = true;
+            label7.Location = new Point(616, 373);
+            label7.Name = "label7";
+            label7.Size = new Size(71, 20);
+            label7.TabIndex = 6;
+            label7.Text = "Ortalama";
+            // 
+            // label8
+            // 
+            label8.AutoSize = true;
+            label8.Location = new Point(786, 373);
+            label8.Name = "label8";
+            label8.Size = new Size(49, 20);
+            label8.TabIndex = 7;
+            label8.Text = "Sonuç";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(22, 396);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(150, 104);
+            listBox1.TabIndex = 8;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.Location = new Point(212, 396);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(150, 104);
+            listBox2.TabIndex = 9;
+            // 
+            // listBox3
+            // 
+            listBox3.FormattingEnabled = true;
+            listBox3.Location = new Point(390, 396);
+            listBox3.Name = "listBox3";
+            listBox3.Size = new Size(150, 104);
+            listBox3.TabIndex = 10;
+            // 
+            // listBox4
+            // 
+            listBox4.FormattingEnabled = true;
+            listBox4.Location = new Point(566, 396);
+            listBox4.Name = "listBox4";
+            listBox4.Size = new Size(150, 104);
+            listBox4.TabIndex = 11;
+            // 
+            // listBox5
+            // 
+            listBox5.FormattingEnabled = true;
+            listBox5.Location = new Point(755, 396);
+            listBox5.Name = "listBox5";
+            listBox5.Size = new Size(150, 104);
+            listBox5.TabIndex = 12;
+            // 
+            // textBox1
+            // 
+            textBox1.Location = new Point(153, 56);
+            textBox1.Name = "textBox1";
+            textBox1.Size = new Size(125, 27);
+            textBox1.TabIndex = 13;
+            // 
+            // textBox2
+            // 
+            textBox2.Location = new Point(130, 131);
+            textBox2.Name = "textBox2";
+            textBox2.Size = new Size(54, 27);
+            textBox2.TabIndex = 14;
+            // 
+            // textBox3
+            // 
+            textBox3.Location = new Point(130, 195);
+            textBox3.Name = "textBox3";
+            textBox3.Size = new Size(54, 27);
+            textBox3.TabIndex = 15;
+            // 
+            // button1
+            // 
+            button1.Font = new Font("Segoe UI", 12F, FontStyle.Bold);
+            button1.Location = new Point(340, 160);
+            button1.Name = "button1";
+            button1.Size = new Size(123, 55);
+            button1.TabIndex = 16;
+            button1.Text = "Hesapla";
+            button1.UseVisualStyleBackColor = true;
+            button1.Click += button1_Click;
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(1204, 584);
+            Controls.Add(button1);
+            Controls.Add(textBox3);
+            Controls.Add(textBox2);
+            Controls.Add(textBox1);
+            Controls.Add(listBox5);
+            Controls.Add(listBox4);
+            Controls.Add(listBox3);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(label8);
+            Controls.Add(label7);
+            Controls.Add(label6);
+            Controls.Add(label5);
+            Controls.Add(label4);
+            Controls.Add(label3);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Name = "Form1";
+            Text = "Form1";
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private Label label1;
+        private Label label2;
+        private Label label3;
+        private Label label4;
+        private Label label5;
+        private Label label6;
+        private Label label7;
+        private Label label8;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private ListBox listBox3;
+        private ListBox listBox4;
+        private ListBox listBox5;
+        private TextBox textBox1;
+        private TextBox textBox2;
+        private TextBox textBox3;
+        private Button button1;
+    }
+}
---
+++ KaldıGeçti/KaldıGeçti/Form1.cs
@@ -0,0 +1,34 @@
+namespace KaldıGeçti
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            int sinav1, sinav2, ortalama;
+
+            sinav1 = Convert.ToInt32(textBox2.Text);
+            sinav2 = Convert.ToInt32(textBox3.Text);
+
+            ortalama = (sinav1 + sinav2) / 2;
+
+            listBox1.Items.Add(textBox1.Text);
+            listBox2.Items.Add(textBox2.Text);
+            listBox3.Items.Add(textBox3.Text);
+            listBox4.Items.Add(ortalama);
+
+            if(ortalama > 50)
+            {
+                listBox5.Items.Add("geçti");
+            }
+            else
+            {
+                listBox5.Items.Add("Kaldı");
+            }
+        }
+    }
+}
