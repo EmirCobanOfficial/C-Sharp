8b72c19256495545c22b86836f17db5d653e0abb
EmirCoban <125053917+EmirCobanOfficial@users.noreply.github.com>
2025-03-24 00:35:39 +0300

    Add files via upload

---
+++ MenuStrip/MenuStrip/Form1.Designer.cs
@@ -0,0 +1,195 @@
+﻿namespace MenuStrip
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            menuStrip1 = new System.Windows.Forms.MenuStrip();
+            shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem = new ToolStripMenuItem();
+            tsueToTsurugiNoWistoriaToolStripMenuItem = new ToolStripMenuItem();
+            aksiyonToolStripMenuItem = new ToolStripMenuItem();
+            shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem1 = new ToolStripMenuItem();
+            maceraToolStripMenuItem = new ToolStripMenuItem();
+            hazurewakuNoJoutaiToolStripMenuItem = new ToolStripMenuItem();
+            puanVerToolStripMenuItem = new ToolStripMenuItem();
+            toolStripComboBox1 = new ToolStripComboBox();
+            richTextBox1 = new RichTextBox();
+            listBox1 = new ListBox();
+            listBox2 = new ListBox();
+            label1 = new Label();
+            label2 = new Label();
+            menuStrip1.SuspendLayout();
+            SuspendLayout();
+            // 
+            // menuStrip1
+            // 
+            menuStrip1.ImageScalingSize = new Size(20, 20);
+            menuStrip1.Items.AddRange(new ToolStripItem[] { shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem, aksiyonToolStripMenuItem, maceraToolStripMenuItem, puanVerToolStripMenuItem });
+            menuStrip1.Location = new Point(0, 0);
+            menuStrip1.Name = "menuStrip1";
+            menuStrip1.Size = new Size(900, 31);
+            menuStrip1.TabIndex = 0;
+            menuStrip1.Text = "menuStrip1";
+            // 
+            // shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem
+            // 
+            shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { tsueToTsurugiNoWistoriaToolStripMenuItem });
+            shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem.Font = new Font("Segoe UI", 10F);
+            shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem.Name = "shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem";
+            shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem.Size = new Size(82, 27);
+            shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem.Text = "Komedi";
+            // 
+            // tsueToTsurugiNoWistoriaToolStripMenuItem
+            // 
+            tsueToTsurugiNoWistoriaToolStripMenuItem.Name = "tsueToTsurugiNoWistoriaToolStripMenuItem";
+            tsueToTsurugiNoWistoriaToolStripMenuItem.Size = new Size(301, 28);
+            tsueToTsurugiNoWistoriaToolStripMenuItem.Text = "Tsue to Tsurugi no Wistoria";
+            tsueToTsurugiNoWistoriaToolStripMenuItem.Click += tsueToTsurugiNoWistoriaToolStripMenuItem_Click;
+            // 
+            // aksiyonToolStripMenuItem
+            // 
+            aksiyonToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem1 });
+            aksiyonToolStripMenuItem.Font = new Font("Segoe UI", 10F);
+            aksiyonToolStripMenuItem.Name = "aksiyonToolStripMenuItem";
+            aksiyonToolStripMenuItem.Size = new Size(82, 27);
+            aksiyonToolStripMenuItem.Text = "Aksiyon";
+            // 
+            // shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem1
+            // 
+            shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem1.Name = "shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem1";
+            shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem1.Size = new Size(723, 28);
+            shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem1.Text = "Shinmai Ossan Boukensha, Saikyou Party ni Shinu hodo Kitaerarete Muteki ni Naru";
+            shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem1.Click += shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem1_Click;
+            // 
+            // maceraToolStripMenuItem
+            // 
+            maceraToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { hazurewakuNoJoutaiToolStripMenuItem });
+            maceraToolStripMenuItem.Font = new Font("Segoe UI", 10.2F, FontStyle.Regular, GraphicsUnit.Point, 162);
+            maceraToolStripMenuItem.Name = "maceraToolStripMenuItem";
+            maceraToolStripMenuItem.Size = new Size(80, 27);
+            maceraToolStripMenuItem.Text = "Macera";
+            // 
+            // hazurewakuNoJoutaiToolStripMenuItem
+            // 
+            hazurewakuNoJoutaiToolStripMenuItem.Name = "hazurewakuNoJoutaiToolStripMenuItem";
+            hazurewakuNoJoutaiToolStripMenuItem.Size = new Size(763, 28);
+            hazurewakuNoJoutaiToolStripMenuItem.Text = "Hazurewaku no Joutai Ijou Skill de Saikyou ni Natta Ore ga Subete wo Juurin suru made";
+            hazurewakuNoJoutaiToolStripMenuItem.Click += hazurewakuNoJoutaiToolStripMenuItem_Click;
+            // 
+            // puanVerToolStripMenuItem
+            // 
+            puanVerToolStripMenuItem.DropDownItems.AddRange(new ToolStripItem[] { toolStripComboBox1 });
+            puanVerToolStripMenuItem.Name = "puanVerToolStripMenuItem";
+            puanVerToolStripMenuItem.Size = new Size(80, 27);
+            puanVerToolStripMenuItem.Text = "Puan Ver";
+            // 
+            // toolStripComboBox1
+            // 
+            toolStripComboBox1.Items.AddRange(new object[] { "5", "4", "3", "2", "1" });
+            toolStripComboBox1.Name = "toolStripComboBox1";
+            toolStripComboBox1.Size = new Size(121, 28);
+            // 
+            // richTextBox1
+            // 
+            richTextBox1.Location = new Point(12, 87);
+            richTextBox1.Name = "richTextBox1";
+            richTextBox1.Size = new Size(653, 312);
+            richTextBox1.TabIndex = 1;
+            richTextBox1.Text = "";
+            // 
+            // listBox1
+            // 
+            listBox1.FormattingEnabled = true;
+            listBox1.Location = new Point(686, 98);
+            listBox1.Name = "listBox1";
+            listBox1.Size = new Size(180, 104);
+            listBox1.TabIndex = 2;
+            // 
+            // listBox2
+            // 
+            listBox2.FormattingEnabled = true;
+            listBox2.Location = new Point(686, 239);
+            listBox2.Name = "listBox2";
+            listBox2.Size = new Size(80, 104);
+            listBox2.TabIndex = 3;
+            // 
+            // label1
+            // 
+            label1.AutoSize = true;
+            label1.Location = new Point(729, 75);
+            label1.Name = "label1";
+            label1.Size = new Size(37, 20);
+            label1.TabIndex = 4;
+            label1.Text = "Film";
+            // 
+            // label2
+            // 
+            label2.AutoSize = true;
+            label2.Location = new Point(705, 216);
+            label2.Name = "label2";
+            label2.Size = new Size(41, 20);
+            label2.TabIndex = 5;
+            label2.Text = "Puan";
+            // 
+            // Form1
+            // 
+            AutoScaleDimensions = new SizeF(8F, 20F);
+            AutoScaleMode = AutoScaleMode.Font;
+            ClientSize = new Size(900, 450);
+            Controls.Add(label2);
+            Controls.Add(label1);
+            Controls.Add(listBox2);
+            Controls.Add(listBox1);
+            Controls.Add(richTextBox1);
+            Controls.Add(menuStrip1);
+            MainMenuStrip = menuStrip1;
+            Name = "Form1";
+            Text = "Form1";
+            menuStrip1.ResumeLayout(false);
+            menuStrip1.PerformLayout();
+            ResumeLayout(false);
+            PerformLayout();
+        }
+
+        #endregion
+
+        private System.Windows.Forms.MenuStrip menuStrip1;
+        private ToolStripMenuItem shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem;
+        private ToolStripMenuItem tsueToTsurugiNoWistoriaToolStripMenuItem;
+        private ToolStripMenuItem aksiyonToolStripMenuItem;
+        private ToolStripMenuItem shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem1;
+        private ToolStripMenuItem maceraToolStripMenuItem;
+        private ToolStripMenuItem hazurewakuNoJoutaiToolStripMenuItem;
+        private ToolStripMenuItem puanVerToolStripMenuItem;
+        private ToolStripComboBox toolStripComboBox1;
+        private RichTextBox richTextBox1;
+        private ListBox listBox1;
+        private ListBox listBox2;
+        private Label label1;
+        private Label label2;
+    }
+}
---
+++ MenuStrip/MenuStrip/Form1.cs
@@ -0,0 +1,31 @@
+namespace MenuStrip
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void tsueToTsurugiNoWistoriaToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Will Serfort, çocukluk arkadaþýna verdiði sözü tutmayý hayal eder ve Büyücü Kulesi'nin tepesinde oturan güçlü büyücülerden biri olan Magia Vander olmayý amaçlar. Ancak, en basit büyüleri bile yapamaz ve bu yüzden Regarden Büyü Akademisi'nde kredi kazanmak için zindan canavarlarýyla savaþmak zorundadýr. Üstelik, bir de zorba bir profesöre karþý kýlýç becerilerini sýnamak zorunda kalýr!";
+            listBox1.Items.Add("Tsue to Tsurugi no Wistoria");
+            listBox2.Items.Add(toolStripComboBox1.Text);
+        }
+
+        private void shinmaiOssanBoukenshaSaikyouPartyNiShinuHodoKitaerareteMutekiNiNaruToolStripMenuItem1_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Bir maceracýnýn iþe olabildiðince genç yaþta baþlamasý gerektiði herkesçe bilinen bir gerçektir, zira maceracýlarýn esas gücünün geldiði sihirli güç genç yaþlarda eðitilmeye baþlanmazsa, ileride çok az geliþtirebilir. Ama Rick Gladiator adýnda bir adam, maceracýlýða otuz-küsür yaþýnda baþlamakla kalmayýp, dünyadaki en güçlü sýnýf, S-sýnýf bir savaþ gücüne sahiptir. Hatta Rick, kýtanýn en güçlü maceracýlarýndan oluþan bir ekibin, efsanevi Orihalcon Fist'in üyeleriyle inanýlmaz bir hayat sürmüþtür. Rick ejderhalardan vampirlere, kelimenin tam anlamýyla \"canavar\" hocalarý tarafýndan eðitilen dövüþ becerileri sayesinde, onu küçümseyen bütün elit maceracýlarý birbiri ardýna yenebilecektir.";
+            listBox1.Items.Add("shinmai Ossan Boukensha Saikyou Party NiShinu Hodo Kitaerarete Muteki NiNaru");
+            listBox2.Items.Add(toolStripComboBox1.Text);
+        }
+
+        private void hazurewakuNoJoutaiToolStripMenuItem_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Text = "Hikaye, Mimori Touka'nýn aniden fantastik bir dünyaya fýrlatýlmasýyla baþlar. Mimori ve sýnýf arkadaþlarý, bu dünyanýn tanrýçasý tarafýndan kahraman olarak hizmet etmeleri için çaðrýlmýþlardýr. Þanslý bir þekilde, öðrencilerin çoðu olaðanüstü yetenekler sergilerken, Mimori'nin yetenekleri sadece E-sýnýfýnda kalýr. Tanrýça, Mimori'nin iþe yaramaz olduðunu düþünerek onu, hiç kimsenin sað çýkamadýðý bir zindana sürgün eder.";
+            listBox1.Items.Add("hazure waku No Joutai");
+            listBox2.Items.Add(toolStripComboBox1.Text);
+        }
+    }
+}
